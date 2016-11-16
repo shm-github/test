aefc5536664d885c026037743d477200d66e397e
hossein <shm.original2@gmail.com>
2016-11-16 12:57:03 +0330

    111

---
+++ app/src/main/java/utils/Const.java
@@ -0,0 +1,12 @@
+package utils;
+
+import android.os.Environment;
+
+/**
+ * Created by Hossein on 11/15/2016.
+ */
+
+public class Const {
+    public static final String APP_DIRECTORY = Environment.getExternalStorageDirectory().getPath() + "/movie_leitner/";
+    public static final String MOVIES_DIRECTORY = APP_DIRECTORY + "movies_part/";
+}
